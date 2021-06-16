e35358cd172ef566c0a77791303db42f5ee5a610
dsilfvast <dustin@silfvast.com>
2021-06-16 16:11:54 -0600

    These files were reverted to original because the file saving routines were broken.

--- app/src/main/java/com/example/boma/ModelToPresenter.java
+++ app/src/main/java/com/example/boma/ModelToPresenter.java
@@ -1,6 +1,5 @@
 package com.example.boma;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface ModelToPresenter {
@@ -10,18 +9,5 @@ public interface ModelToPresenter {
     void ProfileNamesFromModel(List<String> ProfileNames);
     void ProfileDataFromModel(BMIProfile ProfileData);
     void RequestedBMIFromModel(UserBMIData UserData);
-
-    // This is used to group all profiles into a single class for GSON to serialize
-    // all the data into a string
-    class BMIAllProfiles {
-
-        public String LastLoadedProfile;
-        public List<BMIProfile> profile;
-
-        public BMIAllProfiles() {
-            LastLoadedProfile = "";
-            profile = new ArrayList<>();
-        }
-    }
 }
 
