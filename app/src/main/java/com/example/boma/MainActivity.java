685d9e1d0b89d66bd8efb5ae069e678164025bc2
Android Servo <astewart1138@gmail.com>
2021-06-16 22:56:14 -0500

    Edit the New Profile page and add AutoCompleteTextView list with suggested gender options.

--- app/src/main/java/com/example/boma/MainActivity.java
+++ app/src/main/java/com/example/boma/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.boma;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
     MVPPresenter presenter;
+    AutoCompleteTextView autocomplete;
+    String[] gender = { "Male", "Female", "Other"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        autocomplete = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, gender);
+
+        autocomplete.setThreshold(2);
+        autocomplete.setAdapter(adapter);
     }
 }
