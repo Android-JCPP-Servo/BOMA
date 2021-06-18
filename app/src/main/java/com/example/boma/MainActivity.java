41c5d3edae5049515c4fc6277787a66c193e2bd3
Android Servo <astewart1138@gmail.com>
2021-06-18 13:48:34 -0500

    Move Spinner code to SaveNewInfoActivity.java.

--- app/src/main/java/com/example/boma/MainActivity.java
+++ app/src/main/java/com/example/boma/MainActivity.java
@@ -2,11 +2,6 @@ package com.example.boma;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 /**************************
  * NOTE: Need to move Spinner
@@ -14,7 +9,7 @@ import android.widget.Toast;
  * user profile.
  **************************/
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     MVPPresenter presenter;
 
@@ -23,29 +18,11 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         // Create an MVPPresenter if it is needed
         //  Pass a handle to the MainActivity
-        if(presenter == null){
+        if (presenter == null) {
             presenter = new MVPPresenter(this);
         }
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender_array, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getApplicationContext(), "Gender Selected", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
     }
 }
--- app/src/main/java/com/example/boma/SaveNewInfoActivity.java
+++ app/src/main/java/com/example/boma/SaveNewInfoActivity.java
@@ -3,13 +3,36 @@ package com.example.boma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class SaveNewInfoActivity extends AppCompatActivity {
+public class SaveNewInfoActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.save_new_info);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getApplicationContext(), "Gender Selected", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 
 }
