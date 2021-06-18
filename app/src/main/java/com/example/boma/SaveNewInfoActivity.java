2d053106dc09f451905247cae782e0d41159bd7d
dsilfvast <dustin@silfvast.com>
2021-06-18 12:41:44 -0600

    Added SaveNewInfoActivity.java file and associated it with the save_new_info.xml

---
+++ app/src/main/java/com/example/boma/SaveNewInfoActivity.java
@@ -0,0 +1,15 @@
+package com.example.boma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaveNewInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.save_new_info);
+    }
+
+}
