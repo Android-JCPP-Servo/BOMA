9cb6b348658d988adef33c799ac89bf20aa13cd9
dsilfvast <dustin@silfvast.com>
2021-06-29 17:08:25 -0600

    UpdateProfileActivity will get the profile name from the calling intent in onCreate.

--- app/src/main/java/com/team02/boma/UpdateProfileActivity.java
+++ app/src/main/java/com/team02/boma/UpdateProfileActivity.java
@@ -2,14 +2,18 @@ package com.team02.boma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class UpdateProfileActivity extends AppCompatActivity {
+import java.util.List;
+
+public class UpdateProfileActivity extends AppCompatActivity implements MVPListener{
 
     // Every activity that needs access to the MVP structure needs a reference
     // to the global MVPPresenter
     MVPPresenter presenter;
+    String profileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +25,39 @@ public class UpdateProfileActivity extends AppCompatActivity {
             presenter = new MVPPresenter(this.getApplication());
         }
 
+        // Get the intent that started this activity
+        Intent intent = getIntent();
+
+        // Get the profile name that was passed to this intent
+        this.profileName = intent.getStringExtra(MVPView.EXTRA_MESSAGE_PROFILE_NAME);
+
+        // request the profile information from the MVP
+        //  the result is passed to the ProfileDataListener() as a callback
+        presenter.view.RequestProfileData(this, profileName);
+
     }
     public void saveUpdatedProfile(View view) {
         // TODO: we need to pass a profile name to ShowBMIResultsActivity
         presenter.view.ShowBMIResultsActivity("");
+    }
+
+    @Override
+    public void ProfileNamesListener(List<String> profileNames) {
+
+    }
+
+    @Override
+    public void ProfileDataListener(BMIProfile ProfileData) {
+
+    }
+
+    @Override
+    public void UserBMIListener(UserBMIData UserData) {
+
+    }
+
+    @Override
+    public void ProfileCreatedListener(boolean Success) {
+
     }
 }
