75154b0210a3c4dd3ad1d794536cab6d3a175248
dsilfvast <dustin@silfvast.com>
2021-07-08 21:30:36 -0600

    Dial indicator added to BMIResultsActivity

--- app/src/main/java/com/team02/boma/BMIResultsActivity.java
+++ app/src/main/java/com/team02/boma/BMIResultsActivity.java
@@ -14,6 +14,8 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
 
+import pl.droidsonroids.gif.GifImageView;
+
 /***************************
  * NOTES/STEPS TO COMPLETION:
  * -Connect SaveNewInfoActivity to this Activity
@@ -67,13 +69,94 @@ public class BMIResultsActivity extends AppCompatActivity implements MVPListener
     @Override
     public void ProfileDataListener(BMIProfile ProfileData) {
 
+
+        //tempDialString is used for the dial output text
+        String tempDialString = getString(R.string.dial_underweight);
+
+        //tempDialColor is used for the dial output text color
+        // defaults to underweight color
+        int tempDialColor = getResources().getColor(R.color.dial_underweight);
+
+        // Set the angle of rotation for the animated gif based on BMI
+        //  These are underweight values
+        float tempAngle = 36.0f;
+        float offset = ProfileData.lastBMI / 18.5f * 72f;
+
+        // Set Normal weight values
+        if(ProfileData.lastBMI >= 18.5f){
+            tempAngle = 324f;
+            offset = (ProfileData.lastBMI - 18.5f) / 6.5f * 72f;
+            tempDialString = getString(R.string.dial_normal);
+            tempDialColor = getResources().getColor(R.color.dial_normal);
+        }
+
+        // Set Overweight values
+        if(ProfileData.lastBMI >= 25.0f){
+            tempAngle = 252f;
+            offset = (ProfileData.lastBMI - 25.0f) / 5f * 72f;
+            tempDialString = getString(R.string.dial_overweight);
+            tempDialColor = getResources().getColor(R.color.dial_overweight);
+        }
+
+        // Set Obese values
+        if(ProfileData.lastBMI >= 30.0f){
+            tempAngle = 180f;
+            offset = (ProfileData.lastBMI - 30.0f) / 10f * 72f;
+            tempDialString = getString(R.string.dial_obese);
+            tempDialColor = getResources().getColor(R.color.dial_obese);
+        }
+
+        // Set Morbidly Obese values
+        if(ProfileData.lastBMI >= 40.0f){
+            tempAngle = 72f;
+            offset = 0;
+            tempDialString = getString(R.string.dial_morbidly_obese);
+            tempDialColor = getResources().getColor(R.color.dial_morbidly_obese);
+        }
+
+        // set final variables for use in separate threads
+        final float angle = tempAngle - offset;
+        final String DialString = tempDialString;
+        final int DialColor = tempDialColor;
+
         // Since the message is in a TextView, run it on the UiThread
-        // TODO: format the number to limit decimal places
         runOnUiThread(() -> {
             TextView textViewBMI = findViewById(R.id.textViewBMI);
             String userBMI = String.format("%.1f", ProfileData.lastBMI);
             textViewBMI.setText(userBMI);
+            // Rotate dial image
+            GifImageView imageView = (GifImageView) findViewById(R.id.image_dial);
+            imageView.setRotation(angle);
+
+            // set the rotation of the spacer to 0. This will change the Z-order
+            imageView = (GifImageView) findViewById(R.id.image_spacer);
+            imageView.setRotation(0);
+
+            // set the rotation of the needle to 0. This will change the Z-order
+            imageView = (GifImageView) findViewById(R.id.image_needle);
+            imageView.setRotation(0);
         });
+
+        // create a new thread and execute it after a five second delay
+        Thread thread=new Thread(()->{
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Set the dial indicator text and color on the UI thread
+            runOnUiThread(() ->{
+                TextView tvMeter = findViewById(R.id.textViewMeter);
+                tvMeter.setTextColor(DialColor);
+                tvMeter.setText(DialString);
+            });
+
+        });
+
+        // Start the new thread to set the dial text and text color
+        thread.start();
+
 
     }
 
